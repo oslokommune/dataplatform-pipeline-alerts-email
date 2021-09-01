25bb3f29e9e2afe1a5bf0433cec63dccbc91e379
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2021-09-01 15:46:43 +0200

    DP-2245 Use Oslo kommune email API to send emails

--- pipeline_alerts_email/mail.py
+++ pipeline_alerts_email/mail.py
@@ -1,34 +1,31 @@
 from okdata.aws.logging import log_exception
 
-import boto3
-from botocore.exceptions import ClientError
+import os
+import requests
 
 
 def send_email(message, to_address):
-    client = boto3.client("ses", region_name="eu-west-1")
-
     try:
-        return client.send_email(
-            Destination={
-                "ToAddresses": [
-                    to_address,
-                    "simen.heggestoyl@origo.oslo.kommune.no",  # WIP
-                ],
-            },
-            Message={
-                "Body": {
-                    "Text": {
-                        "Charset": "UTF-8",
-                        "Data": message,
-                    },
-                },
-                "Subject": {
-                    "Charset": "UTF-8",
-                    "Data": "Feil ved kjøring av pipeline",
-                },
-            },
-            Source="dataplattform@oslo.kommune.no",
+        # Spec for "EMAIL API" from Team økonomi:
+        # https://developer.oslo.kommune.no/katalog/api/31/introduction
+        payload = {
+            "mottakerepost": [
+                to_address,
+                # "simen.heggestoyl@origo.oslo.kommune.no",  # WIP
+                "jon.morten.kristiansen@origo.oslo.kommune.no",  # WIP
+            ],
+            "avsenderepost": "dataplattform@oslo.kommune.no",
+            "avsendernavn": "Dataplattform",
+            "emne": "Feil ved kjøring av pipeline",
+            "meldingskropp": message,
+        }
+        headers = {"apikey": os.environ["EMAIL_API_KEY"]}
+        response = requests.post(
+            os.environ["EMAIL_API_URL"], data=payload, headers=headers
         )
-    except ClientError as e:
+        response.raise_for_status()
+
+        return {"status_code": response.status_code}
+    except requests.RequestException as e:
         log_exception(e)
         return None
--- test/test_mail.py
+++ test/test_mail.py
@@ -1,16 +1,7 @@
-from unittest.mock import Mock, patch
-
 from pipeline_alerts_email.mail import send_email
 
 
-def test_send_email():
-    response = {"MessageId": 1}
-    mock_client = Mock()
-
-    with patch("pipeline_alerts_email.mail.boto3.client") as client:
-        client.return_value = mock_client
-        mock_client.send_email.return_value = response
-
-        assert send_email("foo", "foo@example.org") == response
-
-        mock_client.send_email.assert_called_once()
+def test_send_email(requests_mock):
+    requests_mock.register_uri("POST", "https://test", text=None, status_code=200)
+    response = {"status_code": 200}
+    assert send_email("foo", "foo@example.org") == response
