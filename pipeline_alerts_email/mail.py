c627cc346bcd7f7d22fa204978a31d46dbec8415
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2021-09-07 13:02:28 +0200

    DP-2245 Add contact info + use BCC for monitoring

--- pipeline_alerts_email/mail.py
+++ pipeline_alerts_email/mail.py
@@ -6,18 +6,27 @@ from okdata.aws.logging import log_exception
 
 def send_email(message, to_address):
     try:
+        html_message = message.replace("\n", "<br />")
+
+        email_html_body = (
+            f"<p>{html_message}</p>"
+            '<p><strong>Ta kontakt med oss på <a href="https://app.slack.com/client/T6W3G5A4C/C01DE13PLDP">Slack</a> eller dataplattform@oslo.kommune.no dersom du trenger hjelp.</strong></p>'
+        )
+
         # Spec for "EMAIL API" from Team økonomi:
         # https://developer.oslo.kommune.no/katalog/api/31/introduction
         payload = {
             "mottakerepost": [
                 to_address,
+            ],
+            "epostbcc": [
                 "simen.heggestoyl@origo.oslo.kommune.no",  # WIP
                 "jon.morten.kristiansen@origo.oslo.kommune.no",  # WIP
             ],
             "avsenderepost": "dataplattform@oslo.kommune.no",
             "avsendernavn": "Dataplattform",
             "emne": "Feil ved kjøring av pipeline",
-            "meldingskropp": message.replace("\n", "<br />"),
+            "meldingskropp": email_html_body,
         }
         headers = {"apikey": os.environ["EMAIL_API_KEY"]}
         response = requests.post(
