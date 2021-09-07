ba4e3ceee83ebe275fde3be66df3133c61505c60
Jon-Morten Kristiansen <jon.morten.kristiansen@origo.oslo.kommune.no>
2021-09-07 13:45:47 +0200

    DP-2245 Use Oslo kommune email API to send emails (#12)
    Sends HTML emails through an [API] by Team økonomi.
    The emails contain the pipeline error message and some contact info for Team dataplattform (Slack and email address).
    
    Temporary monitoring is done through BCC for sent emails.
    
    [API]: https://developer.oslo.kommune.no/katalog/api/31/introduction

--- pipeline_alerts_email/mail.py
+++ pipeline_alerts_email/mail.py
@@ -1,34 +1,40 @@
-from okdata.aws.logging import log_exception
+import os
+import requests
 
-import boto3
-from botocore.exceptions import ClientError
+from okdata.aws.logging import log_exception
 
 
 def send_email(message, to_address):
-    client = boto3.client("ses", region_name="eu-west-1")
-
     try:
-        return client.send_email(
-            Destination={
-                "ToAddresses": [
-                    to_address,
-                    "simen.heggestoyl@origo.oslo.kommune.no",  # WIP
-                ],
-            },
-            Message={
-                "Body": {
-                    "Text": {
-                        "Charset": "UTF-8",
-                        "Data": message,
-                    },
-                },
-                "Subject": {
-                    "Charset": "UTF-8",
-                    "Data": "Feil ved kjøring av pipeline",
-                },
-            },
-            Source="dataplattform@oslo.kommune.no",
+        html_message = message.replace("\n", "<br />")
+
+        email_html_body = (
+            f"<p>{html_message}</p>"
+            '<p><strong>Ta kontakt med oss på <a href="https://app.slack.com/client/T6W3G5A4C/C01DE13PLDP">Slack</a> eller dataplattform@oslo.kommune.no dersom du trenger hjelp.</strong></p>'
         )
-    except ClientError as e:
+
+        # Spec for "EMAIL API" from Team økonomi:
+        # https://developer.oslo.kommune.no/katalog/api/31/introduction
+        payload = {
+            "mottakerepost": [
+                to_address,
+            ],
+            "epostbcc": [
+                "simen.heggestoyl@origo.oslo.kommune.no",  # WIP
+                "jon.morten.kristiansen@origo.oslo.kommune.no",  # WIP
+            ],
+            "avsenderepost": "dataplattform@oslo.kommune.no",
+            "avsendernavn": "Dataplattform",
+            "emne": "Feil ved kjøring av pipeline",
+            "meldingskropp": email_html_body,
+        }
+        headers = {"apikey": os.environ["EMAIL_API_KEY"]}
+        response = requests.post(
+            os.environ["EMAIL_API_URL"], json=payload, headers=headers
+        )
+        response.raise_for_status()
+
+        return {"status_code": response.status_code}
+    except requests.RequestException as e:
         log_exception(e)
         return None
